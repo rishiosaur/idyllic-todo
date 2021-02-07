a193e1e9a5e462bba17653b1f016bfa32c8afc8a
Rishi Kothari <itsrishikothari@gmail.com>
2021-02-07 09:38:37 -0500

    add build switch

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { IdyllicCompiler } from '@idyllic/compiler'
 import { IdyllicServer } from "@idyllic/server"
 
 (async () => {
-	const c = await (await IdyllicCompiler.fromFile('spec.idl')).compile()
+	const c = await (await IdyllicCompiler.fromFile(process.env.NODE_ENV === "production" ? 'build.spec.idl' : "spec.idl")).compile()
 	const server = new IdyllicServer(c)
 	server.start(3000, () => {
 		console.log('Idyllic server started!')
