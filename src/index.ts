0b671c2f86b582b7bf0d651660d005bc06d964b8
Rishi Kothari <itsrishikothari@gmail.com>
2021-02-07 08:58:15 -0500

    update types & package stuffs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { IdyllicCompiler } from '@idyllic/compiler'
-import { IdyllicServer } from '@idyllic/server'
-;(async () => {
+import { IdyllicServer } from "@idyllic/server"
+
+(async () => {
 	const c = await (await IdyllicCompiler.fromFile('spec.idl')).compile()
 	const server = new IdyllicServer(c)
 	server.start(3000, () => {
